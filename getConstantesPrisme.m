2a0f76d1088337a8df473140add1d402863ac254
audrey <audrey.lebelr@gmail.com>
2017-12-01 19:58:06 -0500

    Ajout getConstantesPrisme

---
+++ getConstantesPrisme.m
@@ -0,0 +1,9 @@
+function [dm, rm] = getConstantesPrisme()
+
+% Cette fonction retourne des constantes du cylindre du probleme
+%
+% dm : dimensions du cylindre en mètre
+% rm : centre du prisme en mètre
+
+dm = [0.01, 0.02, 0.05];
+rm = [0.035, 0.04, 0.145];
